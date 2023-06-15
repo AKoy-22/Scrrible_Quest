857222817c952dec3ec259b035054a1412c9b703
Akiko Koyama <akiyama7584@gmail.com>
2023-06-15 11:32:22 -0700

    updated connection to backend

--- src/components/math_garden/MathMain.js
+++ src/components/math_garden/MathMain.js
@@ -7,37 +7,58 @@ import { Link } from 'react-router-dom';
 function MathMain() {
   const canvasRef = useRef(null);
   let ctx;
+  
+  const [num1, setNum1] = useState(); // Initial value for n1
+  const [num2, setNum2] = useState(); // Initial value for n2
 
   useEffect(() => {
     const canvas = canvasRef.current;
     ctx = canvas.getContext('2d', { willReadFrequently: true });
-    prepareCanvas(canvas, ctx);
+    const removeEventListeners = prepareCanvas(canvas, ctx);
+
+    newQuestion();
 
     return () => {
-
+      removeEventListeners(); 
     };
-  }, []);
+  },[]);
+
+  function newQuestion() {
+    const NUM1 = Math.round(Math.random() * 5);
+    setNum1(NUM1);
+    
+    const NUM2 = Math.round(Math.random() * 5);
+    setNum2(NUM2);
+    
+    const answer = NUM1 + NUM2;
+  }
+
+
+
 
   function checkAnswerBtnHandler(){
-    const data = {
-      'name': 'John Doe',
-    };
-  
-    fetch('http://localhost:8000/test', {  
-      method: 'POST', 
+    const canvas = canvasRef.current;
+    const imageDataURL = canvas.toDataURL();
+
+    fetch('http://localhost:8000/process-image/', {
+      method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify({ image: imageDataURL }),
     })
-    .then(response => {
-      // Handle the response from the Django backend
-      console.log(response.status);
-    })
-    .catch(error => {
-      // Handle any errors
-      console.log("There was an error")
-    });
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response from the backend
+        console.log(data);
+        predicted_num = parseInt(data.predicted_number);
+
+
+      })
+      .catch(error => {
+        // Handle any errors
+        console.error(error);
+      });
   }
 
   function eraseBtnHandler(){
@@ -51,8 +72,8 @@ function MathMain() {
       <div className={classes.container}>
 
         <h1>Math Garden</h1>
-        <Link className={classes.homeLink} to="/">Home</Link>
-        <h2 className={classes.question}> <span id='n1'>3</span> + <span id='n2'>2</span> = </h2>
+        <Link className={classes.homeLink} onClick={eraseBtnHandler} to="/">Home</Link>
+        <h2 className={classes.question}> <span id='n1'>{num1}</span> + <span id='n2'>{num2}</span> = </h2>
         <canvas className={classes.myCanvas} ref={canvasRef} width="150" height="150" >error</canvas>
         <div className={classes.topLeftBtns}>
           <button onClick={eraseBtnHandler}>Erase</button>
--- src/components/math_garden/canvas.js
+++ src/components/math_garden/canvas.js
@@ -1,4 +1,4 @@
-const BACK_COLOR = '#000000';
+/*const BACK_COLOR = '#000000';
 const LINE_COL = "#FFFFFF"; 
 const LINE_WIDTH = 10;
 var c;
@@ -68,6 +68,7 @@ export function prepareCanvas(canvas, ctx) {
     canvas.addEventListener('touchcancel', function (event) {
         isDrawing = false;
     });
+    
 }
 
 //drawing on a canvas 
@@ -84,7 +85,114 @@ export function clearCanvas(canvas, ctx){
     currentY = 0;
     prevX = 0;
     prevY = 0;
-    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);    
+    ctx.beginPath();
 }
 
+*/
 
+const BACK_COLOR = '#000000';
+const LINE_COL = "#FFFFFF"; 
+const LINE_WIDTH = 10;
+let currentX = 0;
+let currentY = 0;
+let prevX = 0;
+let prevY = 0;
+let isDrawing = false;
+
+export function prepareCanvas(canvas, ctx) {
+  ctx.fillStyle = BACK_COLOR;
+  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+  ctx.strokeStyle = LINE_COL;
+  ctx.lineWidth = LINE_WIDTH;
+  ctx.lineJoin = 'round';
+
+  function handleMouseMove(event) {
+    if (isDrawing) {
+      prevX = currentX;
+      prevY = currentY;
+      currentX = event.clientX - canvas.offsetLeft;
+      currentY = event.clientY - canvas.offsetTop;
+      draw(ctx);
+    }
+  }
+
+  function handleMouseDown(event) {
+    isDrawing = true;
+    currentX = event.clientX - canvas.offsetLeft;
+    currentY = event.clientY - canvas.offsetTop;
+  }
+
+  function handleMouseUp(event) {
+    isDrawing = false;
+  }
+
+  function handleMouseLeave(event) {
+    isDrawing = false;
+  }
+
+  function handleTouchMove(event) {
+    if (isDrawing) {
+      prevX = currentX;
+      prevY = currentY;
+      currentX = event.touches[0].clientX - canvas.offsetLeft;
+      currentY = event.touches[0].clientY - canvas.offsetTop;
+      draw(ctx);
+    }
+  }
+
+  function handleTouchStart(event) {
+    isDrawing = true;
+    currentX = event.touches[0].clientX - canvas.offsetLeft;
+    currentY = event.touches[0].clientY - canvas.offsetTop;
+  }
+
+  function handleTouchEnd(event) {
+    isDrawing = false;
+  }
+
+  function handleTouchCancel(event) {
+    isDrawing = false;
+  }
+
+  canvas.addEventListener('mousemove', handleMouseMove);
+  canvas.addEventListener('mousedown', handleMouseDown);
+  canvas.addEventListener('mouseup', handleMouseUp);
+  canvas.addEventListener('mouseleave', handleMouseLeave);
+
+  canvas.addEventListener('touchmove', handleTouchMove);
+  canvas.addEventListener('touchstart', handleTouchStart);
+  canvas.addEventListener('touchend', handleTouchEnd);
+  canvas.addEventListener('touchcancel', handleTouchCancel);
+
+  function draw(ctx) {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currentX, currentY);
+    ctx.closePath();
+    ctx.stroke();
+  }
+
+  return function removeEventListeners() {
+    canvas.removeEventListener('mousemove', handleMouseMove);
+    canvas.removeEventListener('mousedown', handleMouseDown);
+    canvas.removeEventListener('mouseup', handleMouseUp);
+    canvas.removeEventListener('mouseleave', handleMouseLeave);
+
+    canvas.removeEventListener('touchmove', handleTouchMove);
+    canvas.removeEventListener('touchstart', handleTouchStart);
+    canvas.removeEventListener('touchend', handleTouchEnd);
+    canvas.removeEventListener('touchcancel', handleTouchCancel);
+  };
+}
+
+export function clearCanvas(canvas, ctx) {
+  currentX = 0;
+  currentY = 0;
+  prevX = 0;
+  prevY = 0;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+}
