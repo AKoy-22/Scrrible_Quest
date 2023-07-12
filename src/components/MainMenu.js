fde01f2449dcbb0fe1967e48a764fe1e8d1672b2
Akiko Koyama <akiyama7584@gmail.com>
2023-07-12 11:50:43 -0700

    star image added

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,10 +2,10 @@ import classes from './css/MainMenu.module.css'
 import { Link } from 'react-router-dom';
 import boygirl from '../images/boy-girl.png'
 import { useState } from 'react';
-
+import star from '../images/girl-star.png'
 function Welcome({handleLogout}){
 /**This component is the main menu page with two options 1. Math and 2.Words */
-    let totScore=localStorage.getItem('currentScore');
+    let totalScore=localStorage.getItem('currentScore');
     console.log("my current score is..."+localStorage.getItem('currentScore'));
     let storedUserName = localStorage.getItem('userStorageName');
     storedUserName = storedUserName ? storedUserName.charAt(0).toUpperCase() + storedUserName.slice(1) : '';
@@ -26,7 +26,11 @@ function Welcome({handleLogout}){
                 <h2 className={classes.welcome}>Hello <span className={classes.userName}>{storedUserName}!</span></h2> 
             </div>
             <div className={classes.boygirl}><img  src={boygirl}/></div>
-            <h3 className={classes.subTitle}>Where do you want to go today ?</h3>
+            <div className={classes.subTitleContainer}>
+                <h3 className={classes.subTitle}>Where do you want to go today ?</h3>
+                {totalScore>=4 && <img className={classes.starImg} src={star} alt="girl on a shooting star"/>}
+            </div>
+           
             <ul className={classes.mainUL}>
                 <li className={classes.mathLink}><Link to="/math-garden-levels">Math Garden</Link></li>
                 <li className={classes.wordLink} ><Link to="/words-river-levels">Words River</Link></li>
